83932e4fdb8782b3a900f5b12725f94ebee92dd8
LigmaF0V <109663182+LigmaF0V@users.noreply.github.com>
2022-07-21 22:50:20 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,25 @@
 
-fx_version 'adamant'
+fx_version 'adamant' -- notify_version
 
-game 'gta5'
+Autor 'LigmaF0V'
+        Autor '!Aesthetic#0104'
+            Autor 'G1ANNHS#9999'
+                Autor '!Nick#0001'
 
-ui_page 'html/form.html'
+Description "THE BEST NOTIFICATION ON FIVEM, YOU DON'T LOSE ANY. TRY IT!. ||>> TriggerEvent('rc_alert:startAlert',2500,"Your notify message here") . For Any help send dm."
+
+
+game 'gta5' -- gamemode 
+
+ui_page 'html/form.html' -- script html
 
 
 
-client_scripts {
+client_scripts {            -- client side  
     'client/main.lua'
 } 
 
-files {
+files {             -- all files to set-up
     'html/form.html',
     'html/css.css',
     'html/jquery-3.4.1.min.js',
@@ -20,6 +28,6 @@ files {
     'html/KorolevMedium.woff',
 }
 
-export "startAlert"
+export "startAlert"     -- export on TriggerEvent('ligma_notify:startAlert',(etc..)
 
-client_script "@Greek_ac/client/injections.lua"
+client_script "@Greek_ac/client/injections.lua" -- replace with your anticheat client side luafile
